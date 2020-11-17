164ba0a158f229c9634b5b050aa903c0d1fd2bdc
Khaled Badenjki <m.k.badenjki2@gmail.com>
2020-11-17 04:34:16 +0200

    add Group to init models

--- notifier/models/__init__.py
+++ notifier/models/__init__.py
@@ -1,6 +1,6 @@
 from notifier.models.user import User
 from notifier.models.blacklist import TokenBlacklist
-from notifier.models.customer import Customer
+from notifier.models.customer import Customer, Group
 
 
-__all__ = ["User", "TokenBlacklist", "Customer"]
+__all__ = ["User", "TokenBlacklist", "Customer", "Group"]
