26a288d72d8f13043c474719ac92fccec1782330
Khaled Badenjki <m.k.badenjki2@gmail.com>
2020-11-18 00:48:32 +0200

    register Device related schema, resource,for swagger

--- notifier/models/device.py
+++ notifier/models/device.py
@@ -5,8 +5,7 @@ class Device(db.Model):
     """Device model for push notifications"""
 
     id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey("customer.id"),
-                            nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey("customer.id"), nullable=False)
     registration_id = db.Column(db.String(512), nullable=True)
     type = db.Column(db.Enum("android", "ios", name="DeviceTypes"), nullable=False)
     version = db.Column(db.String(30), nullable=False)
