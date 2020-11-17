b1bf36e19ddc6b64c1c6ef5ee27ad42723c34895
Khaled Badenjki <m.k.badenjki2@gmail.com>
2020-11-17 21:38:02 +0200

    add Group API docs to swagger and register its schema and resource

--- notifier/api/resources/__init__.py
+++ notifier/api/resources/__init__.py
@@ -1,4 +1,5 @@
 from notifier.api.resources.user import UserResource, UserList
 from notifier.api.resources.customer import CustomerResource, CustomerList
+from notifier.api.resources.group import GroupResource, GroupList
 
-__all__ = ["UserResource", "UserList", "CustomerResource", "CustomerList"]
+__all__ = ["UserResource", "UserList", "CustomerResource", "CustomerList", "GroupResource", "GroupList"]
