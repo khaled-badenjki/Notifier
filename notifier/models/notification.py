0492e642876c76078d159f32dcfa789fa4a0e307
Khaled Badenjki <m.k.badenjki2@gmail.com>
2020-11-18 14:27:21 +0200

    add notification model

---
+++ notifier/models/notification.py
@@ -0,0 +1,20 @@
+from notifier.extensions import db
+import datetime
+
+
+class Notification(db.Model):
+    """Notification model"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(512))
+    status = db.Column(
+        db.Enum("processing", "sent", "failed", name="NotificationStatuses"), default="processing"
+    )
+    type = db.Column(db.Enum("sms", "push", name="NotificationTypes"))
+    customer_id = db.Column(db.Integer, db.ForeignKey("customer.id"))
+    is_dynamic = db.Column(db.Boolean)
+    created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return "<Notification %s>" % self.id
