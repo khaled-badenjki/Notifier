c1d5d884ee8eaf052b448299df8931687a836314
Khaled Badenjki <m.k.badenjki2@gmail.com>
2020-11-19 11:04:16 +0200

    update translations.py to pass lint

--- notifier/translations.py
+++ notifier/translations.py
@@ -1,12 +1,16 @@
-translations = \
-    {
-        "en": {
-            "Dear @calculate_customer_name, you have a @provided_discount % promotion valid till @provided_valid_till. Activate it using this promo code: @provided_promo_code":
-                "Dear @calculate_customer_name, you have a @provided_discount % promotion valid till @provided_valid_till. Activate it using this promo code: @provided_promo_code",
-        },
-
-        "ar": {
-            "Dear @calculate_customer_name, you have a @provided_discount % promotion valid till @provided_valid_till. Activate it using this promo code: @provided_promo_code":
-                "عزيزي @calculate_customer_name، لديك خصم بقيمة @provided_discount بالمية فعال لغاية @provided_valid_till. لتفعيله يرجى ادخال هذا الكود: @provided_promo_code"
-        }
-    }
+translations = {
+    "en": {
+        "Dear @calculate_customer_name, you have a @provided_discount"
+        " % promotion valid till @provided_valid_till. Activate it using "
+        "this promo code: @provided_promo_code": "Dear @calculate_customer_name, you have a @provided_discount % "
+        "promotion valid till @provided_valid_till. Activate it using "
+        "this promo code: @provided_promo_code",
+    },
+    "ar": {
+        "Dear @calculate_customer_name, you have a @provided_discount"
+        " % promotion valid till @provided_valid_till. Activate it using"
+        " this promo code: @provided_promo_code": "عزيزي @calculate_customer_name، لديك خصم بقيمة"
+        " @provided_discount بالمية فعال لغاية @provided_valid_till. "
+        "لتفعيله يرجى ادخال هذا الكود: @provided_promo_code"
+    },
+}
